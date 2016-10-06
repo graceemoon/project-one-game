542a3c8c97fa98f73032a439b2efae5810712cd2
grace moon <hi.gracemoon@gmail.com>
2016-10-06 16:55:15 -0400

    cropped puzzle img, added divs for 9pc puzzle, flexbox for css container

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-// create blank array grid
-// grid should hold 3 separate arrays with 3 into separated by comma
-// create a for loop to pinpoint size or block for coordinate
+// document.addEventListener('DOMContentLoaded', function() {
+//   console.log('this is after the dom loaded');
+// })
 
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('this is after the dom loaded');
-}
+//jquery
+$( document ).ready(function() {
+    console.log( "this is working. YES!!" );
+});
 
 //why?
 //blank grid to store coordinates per puzzle pc
 //that will be created by below nested for loop
-// should be grid[[0,1 0,1, 0,2][1,0 1,1 1,2][2,0 2,1 2,2]]
-grid = [];
+// should be grid[[0,0 0,1, 0,2][1,0 1,1 1,2][2,0 2,1 2,2]]
+var grid = [];
 
 //what is this?
 //creating x & y coordinates on the grid for each puzzle pc
-//e.g. 0,0  0,1  0,2  1,0  1,1  1,2
-for (var i = 0; i < 3; i++) {
+// make each pc 200px b/c puzzle is 600px
+for (var i = 0; i < 10; i++) {
   grid[i] = [];
-  for (var j = 0; j <3; j++) {
+  for (var j = 0; j <10; j++) {
     grid[i][j] = {
-      x: i * 50,
-      y: j * 50
+      x: i*200,
+      y: j*200,
     }
   }
 }
 
+//create a <div> for every coordinate created up to 50x50px?
+// $('.container').append(.innerHTML?)('<div>pc1</div>');
+
+var puzPc = $('.puzzle');
+
+//attach each div to coordinate
+var assignCoord = function() {
+  for (i = 0; i < grid.length; i++) {
+    for(j = 0; j < grid.length[i]; j++) {
+      $('.puzzle').css({
+        'background-position' : grid[i][j].x grid[i][j].y
+      })
+    }
+}
+
+
 //need to make grid break up image
 //randomize puzzles
-//move pieces by clicking the image (will move to blank spot)
-//can only move a block adjacent to the blank space
+//var scramble = [];
 
-//how to move block into the blank space
+
+
+
+
+//puzzle pcs are inside flexbox
 
 //let user know when they win
+
+
+
+
+
+
+
+
+
+
+
+
