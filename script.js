18081b31bc8a85601b4a42d06283ab3823a898b6
grace moon <hi.gracemoon@gmail.com>
2016-10-07 00:16:13 -0400

    created html for game & load pages also js code broke i'm done

--- script.js
+++ script.js
@@ -10,15 +10,35 @@ $( document ).ready(function() {
 //why?
 //blank grid to store coordinates per puzzle pc
 //that will be created by below nested for loop
-// should be grid[[0,0 0,1, 0,2][1,0 1,1 1,2][2,0 2,1 2,2]]
+// should be grid[[1,2,3,4,5,6,7,8,9,10]
+//[1,2,3,4,5,6,8,9,10][1,2,3,4,5,6,7,8,9,10]]
+
 var grid = [];
 
-//what is this?
 //creating x & y coordinates on the grid for each puzzle pc
-// make each pc 200px b/c puzzle is 600px
+// make 60px/pc for 10x10 600px puzzle
+
 for (var i = 0; i < 10; i++) {
+  grid[i] = []
+  //new $('<div class"row">')
+  var newRow = $('<div class="row"></div>');
+  for (var j = 0; j <  10; j++) {
+    var block = $('<div class="puzzle">')
+    var counter = 0;
+    block.attr('id', 'piece'+counter)
+    .css({'background-position' : i*60 + 'px ' + j*60 + 'px'})
+    newRow.append(block)
+  }
+  board.append(newRow)
+}
+
+
+
+// $('.container').append(.innerHTML?)('<div>pc1</div>');
+//for 3 x 3 ---- this works
+for (var i = 0; i < 3; i++) {
   grid[i] = [];
-  for (var j = 0; j <10; j++) {
+  for (var j = 0; j <3; j++) {
     grid[i][j] = {
       x: i*200,
       y: j*200,
@@ -26,20 +46,23 @@ for (var i = 0; i < 10; i++) {
   }
 }
 
-//create a <div> for every coordinate created up to 50x50px?
-// $('.container').append(.innerHTML?)('<div>pc1</div>');
 
-var puzPc = $('.puzzle');
 
-//attach each div to coordinate
-var assignCoord = function() {
-  for (i = 0; i < grid.length; i++) {
-    for(j = 0; j < grid.length[i]; j++) {
-      $('.puzzle').css({
-        'background-position' : grid[i][j].x grid[i][j].y
-      })
-    }
-}
+
+// var puzPc = $('.puzzle');
+
+// //attach each div to coordinate
+// var assignCoord = function() {
+//   for (i = 0; i < grid.length; i++) {
+//     for(j = 0; j < grid.length[i]; j++) {
+//       $('.puzzle').css({
+//         'background-position' : grid[i][j].x grid[i][j].y
+//       })
+//     }
+// }
+
+
+
 
 
 //need to make grid break up image
